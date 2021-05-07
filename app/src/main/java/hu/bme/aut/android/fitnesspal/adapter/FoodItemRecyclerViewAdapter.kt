4f52a312245b564e4df99f4dbe5fe5bea3e508a1
Balázs Sántha <santha.balazs@simonyi.bme.hu>
2021-05-07 09:31:24 +0200

    Add RecyclerView to FoodFragment

---
+++ app/src/main/java/hu/bme/aut/android/fitnesspal/adapter/FoodItemRecyclerViewAdapter.kt
@@ -0,0 +1,80 @@
+package hu.bme.aut.android.fitnesspal.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import hu.bme.aut.android.fitnesspal.databinding.RowFoodBinding
+import hu.bme.aut.android.fitnesspal.model.Food
+
+class FoodItemRecyclerViewAdapter : ListAdapter<Food, FoodItemRecyclerViewAdapter.ViewHolder>(itemCallback){
+
+
+    companion object{
+        object itemCallback : DiffUtil.ItemCallback<Food>(){
+
+            override fun areItemsTheSame(oldItem: Food, newItem: Food): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Food, newItem: Food): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    private var foodList = emptyList<Food>()
+    var itemClickListener: FoodItemClickListener? = null
+
+    interface FoodItemClickListener {
+        fun onItemClick(food: Food)
+        fun onItemLongClick(position: Int, view: View): Boolean
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (
+        RowFoodBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: FoodItemRecyclerViewAdapter.ViewHolder, position: Int) {
+        val food = foodList[position]
+
+        holder.food = food
+
+        holder.binding.tvFoodCalorie.text = food.calorie.toString()
+        holder.binding.tvFoodProtein.text = food.protein.toString()
+        holder.binding.tvFoodCarb.text = food.carb.toString()
+        holder.binding.tvFoodFat.text = food.fat.toString()
+        holder.binding.tvFoodName.text = food.name.toString()
+
+
+        /*val resource = when (todo.priority) {
+            Todo.Priority.LOW -> R.drawable.ic_low
+            Todo.Priority.MEDIUM -> R.drawable.ic_medium
+            Todo.Priority.HIGH -> R.drawable.ic_high
+        }
+        holder.binding.ivPriority.setImageResource(resource)*/
+    }
+
+    fun addAll(_foodList: MutableList<Food>) {
+        foodList += _foodList
+        submitList(foodList)
+    }
+
+
+    inner class ViewHolder(val binding: RowFoodBinding) : RecyclerView.ViewHolder(binding.root) {
+        var food: Food? = null
+
+        init {
+            itemView.setOnClickListener {
+                food?.let { food -> itemClickListener?.onItemClick(food) }
+            }
+
+            itemView.setOnLongClickListener { view ->
+                itemClickListener?.onItemLongClick(adapterPosition, view)
+                true
+            }
+        }
+    }
+
+}
--- app/src/main/java/hu/bme/aut/android/fitnesspal/ui/foods/FoodFragment.kt
+++ app/src/main/java/hu/bme/aut/android/fitnesspal/ui/foods/FoodFragment.kt
@@ -1,20 +1,27 @@
 package hu.bme.aut.android.fitnesspal.ui.foods
 
-import android.os.Binder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import hu.bme.aut.android.fitnesspal.R
-import hu.bme.aut.android.fitnesspal.databinding.ContentScrollingBinding
+import hu.bme.aut.android.fitnesspal.adapter.FoodItemRecyclerViewAdapter
 import hu.bme.aut.android.fitnesspal.databinding.FragmentFoodBinding
+import hu.bme.aut.android.fitnesspal.model.Food
 
-class FoodFragment : Fragment() {
+class FoodFragment : Fragment(), FoodItemRecyclerViewAdapter.FoodItemClickListener {
 
     private lateinit var binding: FragmentFoodBinding
     private lateinit var foodViewModel: FoodViewModel
+    private lateinit var foodItemRecyclerViewAdapter: FoodItemRecyclerViewAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupRecyclerView()
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -31,6 +38,48 @@ class FoodFragment : Fragment() {
             textView.text = it
         })*/
 
+
+        binding.root.findViewById<RecyclerView>(R.id.food_list).adapter = foodItemRecyclerViewAdapter
         return binding.root
     }
+
+
+    private fun setupRecyclerView() {
+        val demoData = mutableListOf(
+                Food(1, "csirkecomb", 500,  20,30,10 ),
+                Food(2, "csokitorta", 800, 15, 78, 42),
+                Food(3, "marhapörkölt", 600, 24, 23, 30),
+                Food(4, "pizza", 1000,  20,30,10 ),
+                Food(5, "banán", 200, 15, 78, 42),
+                Food(6, "palacsinta", 230,  20,30,10 ),
+                Food(7, "túró", 140, 15, 78, 42),
+                Food(8, "kenyér", 360,  20,30,10 ),
+                Food(9, "kolbász", 504, 15, 78, 42),
+                Food(10, "kakaó", 500,  20,30,10 ),
+                Food(11, "narancs", 800, 15, 78, 42),
+                Food(12, "alma", 600, 24, 23, 30),
+                Food(13, "lazac", 1000,  20,30,10 ),
+                Food(14, "karaj", 200, 15, 78, 42),
+                Food(15, "zabpehely", 230,  20,30,10 ),
+                Food(16, "tojás", 140, 15, 78, 42),
+                Food(17, "tej", 360,  20,30,10 ),
+                Food(18, "szaloncukor", 504, 15, 78, 42)
+
+        )
+        foodItemRecyclerViewAdapter = FoodItemRecyclerViewAdapter()
+        foodItemRecyclerViewAdapter.itemClickListener = this
+        foodItemRecyclerViewAdapter.addAll(demoData)
+
+    }
+
+    override fun onItemClick(food: Food) {
+        Log.d("TAG", "short click!")
+    }
+
+    override fun onItemLongClick(position: Int, view: View): Boolean {
+         Log.d("TAG", "long click!")
+            return false
+    }
+
+
 }
